d0d531437422554f748a7f87b9a8133ff4a8a1f7
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-01-15 13:11:12 -0600

    add psuedocode

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,20 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate"); //generte id = red button
 
 //generate password function
 function generatePassword() {
-  console.log("Button has been clicked.")
-}
+  console.log("Button has been clicked."); 
+//1: Prompt user for password criteria
+//  a. password length 8-128
+//  b. lowercase, uppercase, special characters
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate"); //generte id = red button
+//2: Validate user input
+
+//3: Generate password
+
+//4: Display generated password on page
+  return "Generated password will go here."; //PLACEHOLDER RETURN VALUE!!!
+}
 
 // Write password to the #password input displays on password card
 function writePassword() {
