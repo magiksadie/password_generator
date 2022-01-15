aff61d947fa03b374e0e0f2be15159632e633b9d
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-01-15 13:01:36 -0600

    added 'generate password' function

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,13 @@
+
 //generate password function
-
-
-// random # generator
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-};
-
-function setLength() {
-//randNumber window prompt
-  var input = window.prompt("What length would you like your password? Select between 8-128 characters");
-  var inputLength = parseInt(input);
-  //set min, max
-  if (inputLength >= 8 && inputLength <= 128) {
-    var length = inputLength;
-  } else {
-    //else user doesn't input correct value
-    window.alert(
-      "Incorrect Length! You entered: " + //user input
-        inputLength +
-        ". Please enter a number between 8 and 128."
-    );
-    var length = setLength();
-  }
-  return length;
-};
-
-
-
+function generatePassword() {
+  console.log("Button has been clicked.")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); //generte id = red button
 
-// Write password to the #password input
+// Write password to the #password input displays on password card
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
