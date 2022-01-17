71e216e3f1df254851d775ea674dcf4bef496d5d
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-01-16 18:21:38 -0600

    add prompts

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,24 @@ function generatePassword() {
   console.log("Button has been clicked."); 
 //1: Prompt user for password criteria
 //  a. password length 8-128
-//  b. lowercase, uppercase, special characters
-
-//2: Validate user input
+prompt("How long would you like your password? Type a value between 8 - 128.");
+//  b. character types
+//  b. 1. lowercase
+prompt("Would you like to include lowercase? Type 'yes' or 'no'.");
+//  b. 2. uppercase
+prompt("Would you like to include uppercase? Type 'yes' or 'no'.");
+//  b. 3. numeric
+prompt("Would you like to include numeric values? Type 'yes' or 'no'.");
+//  b. 4. special characters
+prompt("How about special characters? '!@#$' Type 'yes' or 'no'.");
+//2: Validate user input. at least one character type should be selected
 
 //3: Generate password
 
 //4: Display generated password on page
   return "Generated password will go here."; //PLACEHOLDER RETURN VALUE!!!
 }
-
+ 
 // Write password to the #password input displays on password card
 function writePassword() {
   var password = generatePassword();
